522278e631557cc2381521f3674ba91b87517d30
Ji Soo Choi <lucy@Jis-MacBook-Pro-2.local>
2020-04-07 16:26:03 -0400

    rebuild page

--- javascript.js
+++ javascript.js
@@ -26,14 +26,6 @@ function halfNumber() {
     alert("Half of "+ inputNum + " "+ "is "+ outputNum);
     console.log("Half of "+ inputNum + " "+ "is "+ outputNum);
     restyle2();
-    // document.getElementById("title2").style.fontSize = "3vmin";
-    // document.getElementById("title2").style.color = "white";
-    // document.getElementById("title2").style.textShadow = "0 0 2vmin white";
-    //
-    // // fortuneButton.style.width = "20vmin";
-    // fortuneButton.style.fontSize= "2.5vmin";
-    // fortuneButton.style.color= "white";
-    // fortuneButton.style.boxShadow= " 0 0 2vmin white";
   }
 }
 
@@ -69,45 +61,8 @@ function fortune () {
   if (fortuneInput == "") {
     alert("Please enter you name.")
   } else {
-    // alert("Dear "+ fortuneInput +", "+ fortuneText[randomFortuneText]);
     fortuneOutput.innerText = ("Dear "+ fortuneInput +", "+ fortuneText[randomFortuneText]);
     restyle();
-    // document.getElementById("title2").style.fontSize = "1em";
-    // document.getElementById("title2").style.color = "#bbb1e3";
-    // document.getElementById("title2").style.textShadow = "none";
-    //
-    // // fortuneButton.style.width = "20vmin";
-    // fortuneButton.style.fontSize= "2vmin";
-    // fortuneButton.style.color= "#211940";
-    // fortuneButton.style.boxShadow= " none";
 
   }
 }
-//////////////////////////////
-
-// halfButton.addEventListener("click", changeToFortune);
-//
-//
-// function changeToFortune() {
-//   document.getElementById("title2").style.fontSize = "3vmin";
-//   document.getElementById("title2").style.color = "white";
-//   document.getElementById("title2").style.textShadow = "0 0 2vmin white";
-//
-//   // fortuneButton.style.width = "20vmin";
-//   fortuneButton.style.fontSize= "2.5vmin";
-//   fortuneButton.style.color= "white";
-//   fortuneButton.style.boxShadow= " 0 0 2vmin white";
-//
-// }
-
-// fortuneButton.addEventListener("click", changeToHalf);
-//
-// function changeToHalf () {
-//   document.getElementById("title").style.fontSize = "3vmin";
-//   document.getElementById("title").style.color = "white";
-//   document.getElementById("title").style.textShadow = "0 0 2vmin white";
-//
-//   // fortuneButton.style.width = "20vmin";
-//   halfButton.style.fontSize= "2.5vmin";
-//   halfButton.style.color= "white";
-//   halfButton.style.boxShadow= " 0 0 2vmin white";
